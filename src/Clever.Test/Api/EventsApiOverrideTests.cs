22832d083eb44b78146a7b3115a47c1794d3c33b
Amelia Jones <amelia.jones@clever.com>
2017-07-30 20:09:07 -0700

    Add separate test file to check that the events override is in place

---
+++ src/Clever.Test/Api/EventsApiOverrideTests.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using RestSharp;
+using NUnit.Framework;
+
+using Clever.Client;
+using Clever.Api;
+using Clever.Model;
+
+namespace Clever.Test
+{
+    /// <summary>
+    ///  Class for testing EventsApi override
+    /// </summary>
+    [TestFixture]
+    public class EventsApiOverrideTests
+    {
+        private EventsApi instance;
+
+        /// <summary>
+        /// Setup before each unit test
+        /// </summary>
+        [SetUp]
+        public void Init()
+        {
+            // Token for Demo App - Sandbox Events
+            Configuration.Default.AccessToken = "a8a5c9c552be70b291572cd3bc993864b4881818";
+            instance = new EventsApi();
+        }
+
+        /// <summary>
+        /// Clean up after each unit test
+        /// </summary>
+        [TearDown]
+        public void Cleanup()
+        {
+
+        }
+
+        /// <summary>
+        /// Test GetEvents loads correct classes
+        /// </summary>
+        [Test]
+        public void GetEventsClassesTest()
+        {
+            int? limit = 1;
+            string startingAfter = null;
+            string endingBefore = null;
+            var response = instance.GetEvents(limit, startingAfter, endingBefore);
+            Assert.IsInstanceOf<EventsResponse> (response, "response expected to be EventsResponse");
+            foreach (var eventObj in response.Data) {
+              Assert.IsInstanceOf<EventResponse> (eventObj, "response.Data[i] object expected to be EventResponse");
+              dynamic eventData = eventObj.Data;
+              string eventDataType = eventData.GetType().ToString();
+              bool isEventClass = eventDataType.Contains("Created") || eventDataType.Contains("Updated") || eventDataType.Contains("Deleted");
+              Assert.IsTrue(isEventClass, "response.Data expected to be event class but is "+eventDataType);
+            }
+        }
+
+    }
+
+}
